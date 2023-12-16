f8643011bf1dd8ce3539a83093629b1275eeae1e
Leon <leong.li@hotmail.com>
2023-12-16 13:09:12 -0500

    adjusted item page, still needs text align top, account & cart logo missing, & back button missing

--- EECS4413FinalProject/itempage.php
+++ EECS4413FinalProject/itempage.php
@@ -1,48 +1,66 @@
-<?php 
+<?php
 
-    
-    require('backend/dao/itemDAOImpl.php');
-    
-    if(isset($_GET['id']) ){
-        $id = $_GET['id'];
-    } else {
-        header("Location: index.php" );
-    }
-    
-    $config = 'config/config.php';
-    $db = 'config/db.php';
-    $dao = new itemDAOImpl();
-    $data = $dao->getItemsById($id, $config, $db);
+
+require('backend/dao/itemDAOImpl.php');
+
+if (isset($_GET['id'])) {
+    $id = $_GET['id'];
+} else {
+    header("Location: index.php");
+}
+
+$config = 'config/config.php';
+$db = 'config/db.php';
+$dao = new itemDAOImpl();
+$data = $dao->getItemsById($id, $config, $db);
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Item Page</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;500;700&display=swap" rel="stylesheet">
 </head>
+
 <body>
     <?php include 'header.php' ?>
 
     <!-- Content -->
-    <section>
-        <?php foreach($data as $item) :?>
-            <form action="<?php echo htmlspecialchars("backend/controller/orderCon.php"); ?>" method="post">
-            <div>
-                <img src="<?php echo $item['ImageURL'] ?>" alt="Product">
-                <div>Name: <?php echo $item['Name'] ?></div><br>
-                <div>Price: <?php echo $item['Price'] ?></div><br>
-                <div>Description: <?php echo $item['Description'] ?></div><br>
-                <div>Brand: <?php echo $item['Brand'] ?></div><br>
+    <section style="height:90vh">
+        <?php foreach ($data as $item) : ?>
+            <div class="container h-75">
+                <form action="<?php echo htmlspecialchars("backend/controller/orderCon.php"); ?>" method="post" class="row h-100  align-items-center ">
+                    <div class="col-md-4">
+                        <img src="<?php echo $item['ImageURL'] ?>" alt="Product" class="img-fluid">
+                    </div>
+                    <div class="col-md-8" style="font-family: 'Open Sans', sans-serif;">
+                        <div style="font-weight:700; font-size:xx-large" class="pb-0">Name: <?php echo $item['Name'] ?></div>
+                        <div style="font-weight:700; font-size:large">Brand: <?php echo $item['Brand'] ?></div><br>
+                        <div>Price: $<?php echo $item['Price'] ?></div><br>
+                        <div>Description: <?php echo $item['Description'] ?></div><br>
+                       
+                        <label for="">Qty: </label><input type="number" name="qty" min="1" max="999" value="1">
+                        <input type="hidden" name="id" value="<?php echo $item['ItemID']; ?>">
+                        <input type="submit" name="add" value="Add to Cart">
+                    </div>
+                </form>
             </div>
-            <label for="">Qty: </label><input type="number" name="qty" min="1" max="999" value="1">
-            <input type="hidden" name="id" value="<?php echo $item['ItemID']; ?>">
-            <input type="submit" name="add" value="Add to Cart">
-            </form>
+
+
         <?php endforeach; ?>
     </section>
 
     <?php include 'footer.php' ?>
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+
 </body>
+
 </html>
